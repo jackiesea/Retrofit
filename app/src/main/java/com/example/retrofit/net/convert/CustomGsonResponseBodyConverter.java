ce0d372a0d60789df1c43b57cd8a86b4fbf01fa9
Mr.CSH <547372220@qq.com>
2020-03-31 21:14:57 +0800

    Update CustomGsonResponseBodyConverter.java

--- app/src/main/java/com/example/retrofit/net/convert/CustomGsonResponseBodyConverter.java
+++ app/src/main/java/com/example/retrofit/net/convert/CustomGsonResponseBodyConverter.java
@@ -6,17 +6,30 @@ import com.example.retrofit.net.ApiException;
 import com.example.retrofit.net.BaseBean;
 import com.example.retrofit.net.GsonUtils;
 import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 
+import okhttp3.MediaType;
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class CustomGsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+    private Gson gson;
+    private TypeAdapter<T> adapter;
 
-    CustomGsonResponseBodyConverter(Gson gson, TypeToken<T> typeToken) {
-
+    CustomGsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+        this.gson = gson;
+        this.adapter = adapter;
     }
 
     @Override
@@ -29,7 +42,18 @@ public class CustomGsonResponseBodyConverter<T> implements Converter<ResponseBod
             throw new ApiException(obj.getCode(), obj.getMsg());
         }
         value.close();
-        return new Gson().fromJson(json, new TypeToken<BaseBean>() {
-        }.getType());
+        /*return new Gson().fromJson(json, new TypeToken<BaseBean<T>>() {
+        }.getType());*/
+        MediaType contentType = value.contentType();
+        Charset charset = contentType != null ? contentType.charset(UTF_8) : UTF_8;
+        InputStream inputStream = new ByteArrayInputStream(json.getBytes());
+        Reader reader = new InputStreamReader(inputStream, charset);
+        JsonReader jsonReader = gson.newJsonReader(reader);
+
+        try {
+            return adapter.read(jsonReader);
+        } finally {
+            value.close();
+        }
     }
 }
